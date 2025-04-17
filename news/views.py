0193dc8d98b518e3a40446ed84f1236012b535a0
aslan-kayl <aslankaboev60@gmail.com>
2025-04-17 08:44:21 +0500

    add endpoint category and view_news

--- news/views.py
+++ news/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
-
+from .models import News, Category
 # Create your views here.
 
 
 def index(request):
     news = News.objects.all()
-    context = {'news': news,
-               'title': 'Список новостей'
-    }
+    context = {
+               'news': news,
+               'title': 'Список новостей',
+            }
     return render(request, template_name='news/index.html', context=context)
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'category': category})
+
+def view_news(request, news_id):
+     news_item= News.objects.get(pk=news_id)
+     return render(request, 'news/view_news.html', {'news_item': news_item})
